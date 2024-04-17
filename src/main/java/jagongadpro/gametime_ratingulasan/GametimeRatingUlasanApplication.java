ed0cc9b8a6b471b3241d3729f93713a90e8c6d24
fathonidf <daffafathoni@gmail.com>
2024-04-17 14:15:05 +0700

    Initialize Spring Boot

---
+++ src/main/java/jagongadpro/gametime_ratingulasan/GametimeRatingUlasanApplication.java
@@ -0,0 +1,13 @@
+package jagongadpro.gametime_ratingulasan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GametimeRatingUlasanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GametimeRatingUlasanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jagongadpro/gametime_ratingulasan/GametimeRatingUlasanApplicationTests.java
@@ -0,0 +1,13 @@
+package jagongadpro.gametime_ratingulasan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GametimeRatingUlasanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
