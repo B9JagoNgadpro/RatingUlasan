155f62043eeb92dd155049b959d24b71825628fb
fathonidf <daffafathoni@gmail.com>
2024-04-17 15:04:31 +0700

    Add Deployment Files

--- src/main/java/jagongadpro/gametime_ratingulasan/GametimeRatingUlasanApplication.java
+++ src/main/java/jagongadpro/gametime_ratingulasan/GametimeRatingUlasanApplication.java
@@ -2,8 +2,9 @@ package jagongadpro.gametime_ratingulasan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class GametimeRatingUlasanApplication {
 
     public static void main(String[] args) {
