3fc8bd29e2d0e814d9d82d37d00c910b372c4c3b
fathonidf <daffafathoni@gmail.com>
2024-04-24 02:15:45 +0700

    [GREEN] Implementing model TanggapanUlasan and validation

---
+++ src/main/java/jagongadpro/gametime_ratingulasan/model/TanggapanUlasan.java
@@ -0,0 +1,31 @@
+package jagongadpro.gametime_ratingulasan.model;
+
+import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TanggapanUlasan {
+    String id;
+    String penjualId;
+    Ulasan ulasan;
+    String tanggapan;
+    LocalDate date;
+
+    public void setTanggapan(String tanggapan) {
+        if (tanggapan != null && !tanggapan.isEmpty()) {
+            this.tanggapan = tanggapan;
+        } else {
+            throw new IllegalArgumentException("Tanggapan tidak boleh kosong");
+        }
+    }
+
+    public void setUlasan(Ulasan ulasan) {
+        if (ulasan != null) {
+            this.ulasan = ulasan;
+        } else {
+            throw new IllegalArgumentException("Ulasan tidak boleh kosong");
+        }
+    }
+}
