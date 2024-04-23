6475be9ddf04d08709192085e71fe84145b0123c
fathonidf <daffafathoni@gmail.com>
2024-04-24 02:14:26 +0700

    [GREEN] Implementing Description and Rating validation on model Ulasan

--- src/main/java/jagongadpro/gametime_ratingulasan/model/Ulasan.java
+++ src/main/java/jagongadpro/gametime_ratingulasan/model/Ulasan.java
@@ -13,4 +13,26 @@ public class Ulasan {
     Integer rating;
     String deskripsi;
     LocalDate date;
+
+    public void setDeskripsi(String deskripsi) {
+        if (deskripsi != null && !deskripsi.isEmpty()) {
+            this.deskripsi = deskripsi;
+        } else {
+            throw new IllegalArgumentException("Deskripsi tidak boleh kosong");
+        }
+    }
+
+    public void setRating(Integer rating) {
+        if (rating != null) {
+            // Memastikan rating berada dalam rentang 1-5
+            if (rating < 1) {
+                this.rating = 1;
+            } else if (rating > 5) {
+                this.rating = 5;
+            } else {
+                this.rating = rating;
+            }
+        }
+    }
 }
+
