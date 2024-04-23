bec7698a9bfd0b6b0642a1b15124e3a88a2a83cb
fathonidf <daffafathoni@gmail.com>
2024-04-23 13:43:59 +0700

    [RED] make Game model test

---
+++ src/main/java/jagongadpro/gametime_ratingulasan/model/Game.java
@@ -0,0 +1,14 @@
+package jagongadpro.gametime_ratingulasan.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Game {
+
+}
---
+++ src/test/java/jagongadpro/gametime_ratingulasan/model/GameTest.java
@@ -0,0 +1,49 @@
+package jagongadpro.gametime_ratingulasan.model;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+
+class GameTest {
+    Game game = new Game();
+
+    @Test
+    void testGetterSetterNama(){
+        game.setNama("Game1");
+        assertEquals("Game1", game.getNama());
+    }
+
+    @Test
+    void testGetterSetterId(){
+        game.setId("abc");
+        assertEquals("abc", game.getId());
+    }
+    @Test
+    void testGetterSetterStok(){
+        game.setStok(10);
+        assertEquals(10, game.getStok());
+    }
+
+    @Test
+    void testGetterSetterHarga(){
+        game.setHarga(1000);
+        assertEquals(1000, game.getHarga());
+    }
+
+    @Test
+    void testGetterSetterDeskripsi(){
+        game.setDeskripsi("bagus");
+        assertEquals("bagus", game.getDeskripsi());
+    }
+    @Test
+    void testGetterSetterKategori(){
+        game.setKategori("action");
+        assertEquals("action", game.getKategori());
+    }
+
+
+}
