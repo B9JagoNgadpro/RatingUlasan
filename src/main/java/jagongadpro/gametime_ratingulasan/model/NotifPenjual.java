efc318ca41a66ff34b9a9f6388bf2e2c5d8ec5b3
fathonidf <daffafathoni@gmail.com>
2024-04-24 16:10:49 +0700

    [GREEN] Implements NotifPenjual model for Observer Pattern

---
+++ src/main/java/jagongadpro/gametime_ratingulasan/model/NotifPenjual.java
@@ -0,0 +1,11 @@
+package jagongadpro.gametime_ratingulasan.model;
+
+import java.util.ArrayList;
+import lombok.*;
+
+@Setter
+@Getter
+public class NotifPenjual {
+    String idPenjual;
+    ArrayList<String> notifs;
+}
