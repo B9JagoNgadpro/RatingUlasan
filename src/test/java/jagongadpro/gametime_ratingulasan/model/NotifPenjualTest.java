edd858e59d04bbdd35af5e2b7e5ce347bd201570
fathonidf <daffafathoni@gmail.com>
2024-04-24 16:09:54 +0700

    [RED] Add tests for model NotifPenjual

---
+++ src/test/java/jagongadpro/gametime_ratingulasan/model/NotifPenjualTest.java
@@ -0,0 +1,53 @@
+package jagongadpro.gametime_ratingulasan.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+
+public class NotifPenjualTest {
+    private NotifPenjual notifPenjual;
+    private ArrayList<String> notifikasi;
+
+    @BeforeEach
+    void setUp() {
+        this.notifikasi = new ArrayList<>();
+        notifPenjual = new NotifPenjual();
+
+        String notif1 = "User123 telah mengulas produk game123 kamu!";
+        String notif2 = "User456 telah mengulas produk game123 kamu!";
+        String notif3 = "User789 telah mengulas produk game456 kamu!";
+        notifikasi.add(notif1);
+        notifikasi.add(notif2);
+        notifikasi.add(notif3);
+    }
+
+    @Test
+    public void testConstructor() {
+        NotifPenjual newNotifPenjual = new NotifPenjual();
+        assertNotNull(newNotifPenjual);
+    }
+
+    @Test
+    public void testSetterGetter() {
+        notifPenjual.setIdPenjual("penjual123");
+        assertEquals("penjual123", notifPenjual.getIdPenjual());
+    }
+
+    @Test
+    public void testEmptyNotifications() {
+        notifikasi.clear();
+        notifPenjual.setNotifs(notifikasi);
+        assertFalse(!notifPenjual.getNotifs().isEmpty());
+    }
+
+    @Test
+    public void testListNotifications() {
+        notifPenjual.setNotifs(notifikasi);
+        assertArrayEquals(notifikasi.toArray(), notifPenjual.getNotifs().toArray());
+    }
+}
