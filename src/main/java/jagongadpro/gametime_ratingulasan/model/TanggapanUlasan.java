47b0c57968f2334d2c0617d3edac0590844fb12a
fathonidf <daffafathoni@gmail.com>
2024-04-30 23:05:59 +0700

    [GREEN] Implementing TanggapanUlasan model with Builder pattern

--- src/main/java/jagongadpro/gametime_ratingulasan/model/TanggapanUlasan.java
+++ src/main/java/jagongadpro/gametime_ratingulasan/model/TanggapanUlasan.java
@@ -1,36 +1,68 @@
 package jagongadpro.gametime_ratingulasan.model;
 
+import lombok.Getter;
+import lombok.Setter;
 import java.time.LocalDate;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
 public class TanggapanUlasan {
-    String id;
-    String penjualId;
-    Ulasan ulasan;
-    String tanggapan;
-    LocalDate date;
+    private String id;
+    private String penjualId;
+    private Ulasan ulasan;
+    private String tanggapan;
+    private LocalDate date;
 
-    public void setTanggapan(String tanggapan) {
-        if (tanggapan != null && !tanggapan.isEmpty()) {
-            this.tanggapan = tanggapan;
-        } else {
-            throw new IllegalArgumentException("Tanggapan tidak boleh kosong");
-        }
+    private TanggapanUlasan(Builder builder) {
+        this.id = builder.id;
+        this.penjualId = builder.penjualId;
+        this.ulasan = builder.ulasan;
+        this.tanggapan = builder.tanggapan;
+        this.date = builder.date;
     }
 
-    public void setUlasan(Ulasan ulasan) {
-        if (ulasan != null) {
-            this.ulasan = ulasan;
-        } else {
-            throw new IllegalArgumentException("Ulasan tidak boleh kosong");
+    public static class Builder {
+        private String id;
+        private String penjualId;
+        private Ulasan ulasan;
+        private String tanggapan;
+        private LocalDate date;
+
+        public Builder id(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder penjualId(String penjualId) {
+            this.penjualId = penjualId;
+            return this;
+        }
+
+        public Builder ulasan(Ulasan ulasan) {
+            if (ulasan != null) {
+                this.ulasan = ulasan;
+            } else {
+                throw new IllegalArgumentException("Ulasan tidak boleh kosong");
+            }
+            return this;
+        }
+
+        public Builder tanggapan(String tanggapan) {
+            if (tanggapan != null && !tanggapan.isEmpty()) {
+                this.tanggapan = tanggapan;
+            } else {
+                throw new IllegalArgumentException("Tanggapan tidak boleh kosong");
+            }
+            return this;
+        }
+
+        public Builder date(LocalDate date) {
+            this.date = date;
+            return this;
+        }
+
+        public TanggapanUlasan build() {
+            return new TanggapanUlasan(this);
         }
     }
 }
