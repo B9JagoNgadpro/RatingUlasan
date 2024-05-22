04082b7c79fb532a773ba549102256d5fa3dc8f6
fathonidf <daffafathoni@gmail.com>
2024-05-22 22:58:00 +0700

    [REFACTOR] deleting unused imports

--- src/main/java/jagongadpro/gametime_ratingulasan/GametimeRatingUlasanApplication.java
+++ src/main/java/jagongadpro/gametime_ratingulasan/GametimeRatingUlasanApplication.java
@@ -2,7 +2,6 @@ package jagongadpro.gametime_ratingulasan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication()
 public class GametimeRatingUlasanApplication {
