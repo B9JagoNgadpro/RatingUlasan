341ae35fa1c21f28ebad9c09e452fbd8e43509d3
fathonidf <daffafathoni@gmail.com>
2024-05-22 22:55:34 +0700

    [RED] increasing test coverage for model

--- src/test/java/jagongadpro/gametime_ratingulasan/model/UlasanTest.java
+++ src/test/java/jagongadpro/gametime_ratingulasan/model/UlasanTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class UlasanTest {
+class UlasanTest {
     private Ulasan ulasan;
 
     @BeforeEach
@@ -21,12 +21,18 @@ public class UlasanTest {
     }
 
     @Test
-    public void testConstructor() {
+     void testDefaultConstructor(){
+        Ulasan newUlasan = new Ulasan();
+        assertNotNull(newUlasan);
+    }
+
+    @Test
+     void testConstructor() {
         assertNotNull(ulasan);
     }
 
     @Test
-    public void testGetter() {
+     void testGetter() {
         assertEquals("ulasan1", ulasan.getId());
         assertEquals("user123", ulasan.getIdUser());
         assertEquals(4, ulasan.getRating().intValue());
@@ -37,7 +43,7 @@ public class UlasanTest {
     }
 
     @Test
-    public void testValidationRating() {
+     void testValidationRatingLow() {
         Ulasan testRatingUlasan = new Ulasan.Builder()
                                 .rating(0) // Invalid rating
                                 .build();
@@ -46,12 +52,47 @@ public class UlasanTest {
     }
 
     @Test
-    public void testValidationDescription() {
+     void testValidationRatingHigh() {
+        Ulasan testRatingUlasan = new Ulasan.Builder()
+                .rating(10) // Invalid rating
+                .build();
+
+        assertEquals(5, testRatingUlasan.getRating().intValue());
+    }
+
+    @Test
+     void testValidationRatingNull() {
         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
             new Ulasan.Builder()
-                    .deskripsi("") // Empty description
+                    .rating(null) // Empty description
                     .build();
         });
-        assertTrue(exception.getMessage().contains("Deskripsi tidak boleh kosong"));
+        assertTrue(exception.getMessage().contains("Rating tidak boleh kosong"));
+    }
+
+    @Test
+     void testNullDescription() {
+        Ulasan testDescriptionUlasan = new Ulasan.Builder()
+                    .deskripsi(null) // Empty description
+                    .build();
+        assertEquals("", testDescriptionUlasan.getDeskripsi());
+    }
+
+    @Test
+     void testFilledDescription() {
+        Ulasan testDescriptionUlasan = new Ulasan.Builder()
+                .deskripsi("Wow keren!") // Empty description
+                .build();
+        assertEquals("Wow keren!", testDescriptionUlasan.getDeskripsi());
+    }
+
+    @Test
+     void testChangeDescription() {
+        Ulasan testDescriptionUlasan = new Ulasan.Builder()
+                .deskripsi("Wow keren!") // Empty description
+                .build();
+
+        testDescriptionUlasan.setDeskripsi("Biasa aja deng");
+        assertEquals("Biasa aja deng", testDescriptionUlasan.getDeskripsi());
     }
 }
