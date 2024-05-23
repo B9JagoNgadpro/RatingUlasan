2bf4b824e201e1bcd61f197d84d646b531222ba2
fathonidf <daffafathoni@gmail.com>
2024-05-24 02:48:01 +0700

    [GREEN] Implementing Async for TanggapanUlasanService

--- src/main/java/jagongadpro/gametime_ratingulasan/service/TanggapanUlasanService.java
+++ src/main/java/jagongadpro/gametime_ratingulasan/service/TanggapanUlasanService.java
@@ -1,31 +1,17 @@
 package jagongadpro.gametime_ratingulasan.service;
 
 import jagongadpro.gametime_ratingulasan.model.TanggapanUlasan;
+
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 
 public interface TanggapanUlasanService {
-    TanggapanUlasan createTanggapanUlasan(TanggapanUlasan tanggapanUlasan);
-    TanggapanUlasan updateTanggapanUlasan(TanggapanUlasan tanggapanUlasan);
-    void deleteTanggapanUlasan(String id);
-    Optional<TanggapanUlasan> findTanggapanUlasanById(String id);
-    Optional<TanggapanUlasan> findTanggapanUlasanByUlasanId(String ulasanId);
-    List<TanggapanUlasan> findAllTanggapanUlasan();
-    List<TanggapanUlasan> findAllTanggapanUlasanByPenjualId(String penjualId);
+    CompletableFuture<TanggapanUlasan> createTanggapanUlasan(TanggapanUlasan tanggapanUlasan);
+    CompletableFuture<TanggapanUlasan> updateTanggapanUlasan(TanggapanUlasan tanggapanUlasan);
+    CompletableFuture<Void> deleteTanggapanUlasan(String id);
+    CompletableFuture<Optional<TanggapanUlasan>> findTanggapanUlasanById(String id);
+    CompletableFuture<Optional<TanggapanUlasan>> findTanggapanUlasanByUlasanId(String ulasanId);
+    CompletableFuture<List<TanggapanUlasan>> findAllTanggapanUlasan();
+    CompletableFuture<List<TanggapanUlasan>> findAllTanggapanUlasanByPenjualId(String penjualId);
 }
-
-
-//package jagongadpro.gametime_ratingulasan.service;
-//
-//import jagongadpro.gametime_ratingulasan.model.TanggapanUlasan;
-//import java.util.List;
-//
-//public interface TanggapanUlasanService {
-//    TanggapanUlasan createTanggapanUlasan(TanggapanUlasan tanggapanUlasan);
-//    TanggapanUlasan updateTanggapanUlasan(TanggapanUlasan tanggapanUlasan);
-//    void deleteTanggapanUlasan(String id);
-//    TanggapanUlasan findTanggapanUlasanById(String id);
-//    TanggapanUlasan findTanggapanUlasanByUlasanId(String ulasanId);
-//    List<TanggapanUlasan> findAllTanggapanUlasan();
-//    List<TanggapanUlasan> findAllTanggapanUlasanByPenjualId(String penjualId);
-//}
