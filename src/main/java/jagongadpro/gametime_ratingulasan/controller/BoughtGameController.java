5d785ba5a366d980c4120daccc0d98fc01708987
fathonidf <daffafathoni@gmail.com>
2024-05-26 23:04:07 +0700

    [REFACTOR] refactoring boughtgame controller

--- src/main/java/jagongadpro/gametime_ratingulasan/controller/BoughtGameController.java
+++ src/main/java/jagongadpro/gametime_ratingulasan/controller/BoughtGameController.java
@@ -5,6 +5,7 @@ import jagongadpro.gametime_ratingulasan.service.BoughtGameService;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.Map;
@@ -15,12 +16,14 @@ import java.util.Map;
 public class BoughtGameController {
 
     private final BoughtGameService service;
+    private final RestTemplate restTemplate;
 
-    private BoughtGameController(BoughtGameService service) {
+    public BoughtGameController(BoughtGameService service, RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
         this.service = service;
     }
 
-    public BoughtGame inputBoughtGame(@RequestBody Map<String, Object> data) {
+    public BoughtGame inputBoughtGame(Map<String, Object> data) {
         BoughtGame boughtGame = new BoughtGame(
                 data.get("idGame").toString(),
                 data.get("idUser").toString()
@@ -29,27 +32,40 @@ public class BoughtGameController {
     }
 
     @GetMapping("/user/{idUser}")
-    public ResponseEntity<List<BoughtGame>> getBoughtGameUser(@PathVariable String idUser){
+    public ResponseEntity<List<BoughtGame>> getBoughtGameUser(@PathVariable String idUser) {
         List<BoughtGame> boughtGames = service.getBoughtGamesByUserId(idUser);
         return ResponseEntity.ok(boughtGames);
     }
 
     @PatchMapping("/isReviewed/user/{idUser}")
     public BoughtGame boughtGameReviewed(@PathVariable String idUser, @RequestBody Map<String, Object> data) {
-        BoughtGame boughtGameReviewed = service.boughtGameReviewed(data.get("idGame").toString(), idUser);
-        return boughtGameReviewed;
+        return service.boughtGameReviewed(data.get("idGame").toString(), idUser);
     }
 
     @PatchMapping("/unReviewed/user/{idUser}")
     public BoughtGame boughtGameUnReviewed(@PathVariable String idUser, @RequestBody Map<String, Object> data) {
-        BoughtGame boughtGameReviewed = service.boughtGameUnReviewed(data.get("idGame").toString(), idUser);
-        return boughtGameReviewed;
+        return service.boughtGameUnReviewed(data.get("idGame").toString(), idUser);
     }
 
-    @PostMapping("/input")
-    public void fetchBoughtGame(@RequestBody List<Map<String, Object>> gamesData) {
-        for (Map<String, Object> gameData : gamesData) {
-            inputBoughtGame(gameData);
+    @PostMapping("/input/{userId}")
+    public void fetchBoughtGame(@PathVariable String userId) {
+        String url = "https://gametimeriwayat-production.up.railway.app/get/" + userId;
+
+        // Fetch data from the URL
+        List<Map<String, Object>> response = restTemplate.getForObject(url, List.class);
+
+        // Process each game's data
+        for (Map<String, Object> data : response) {
+            List<Map<String, Object>> games = (List<Map<String, Object>>) data.get("games");
+            String pembeliId = data.get("pembeli_id").toString();
+            for (Map<String, Object> gameData : games) {
+                // Create a data map to pass to inputBoughtGame
+                Map<String, Object> boughtGameData = Map.of(
+                        "idGame", gameData.get("id"),
+                        "idUser", pembeliId
+                );
+                inputBoughtGame(boughtGameData);
+            }
         }
     }
 }
